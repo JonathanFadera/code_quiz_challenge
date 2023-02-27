c22969d5cdc68f869f5249b08d8ba8f5dee4f1f4
AlbusSeverus <ericsonhnc@gmail.com>
2023-02-26 16:15:19 -0800

    init repo

---
+++ assets/javascript/script.js
@@ -0,0 +1,54 @@
+var questions = [
+    {
+        question:"What tag is used to define – and place – an interactive button in an HTML document?",
+        answers: ["<button>", "<interactive>", "<form>", "<input>"],
+        correctAnswer: "<button>"
+    },
+    
+    {
+        question:"What tag is used to render or transform text into an emphasized (italics) version?",
+        answers: ["<a>", "<blockquote>", "<em>", "<strong>"],
+        correctAnswer: "<em>"
+    },
+
+    {
+        question:"What declaration MUST be included as the first item in an HTML document before the tag and is used to provide instructions to the web browser?",
+        answers: ["<caption>", "<code>", "<!DOCTYPE>", "<html>"],
+        correctAnswer: "<!DOCTYPE>"
+    },
+
+    {
+        question:"What is the name of the property that is used to define the special state of an element?",
+        answers: ["Style", "Syntax", "Pseudo-class", "Alignment"],
+        correctAnswer: "Pseudo-class"
+    },
+
+    {
+        question:"What is the box called in CSS that wraps around every HTML element?",
+        answers: ["Float", "Margin", "Wrap", "Box Model"],
+        correctAnswer: "Box Model"
+    },
+
+    {
+        question:"What is the name of CSS design that calls for fluid and adaptable elements based on the device resolution or size?",
+        answers: ["Evolution", "Shifting", "Responsive", "Cascading"],
+        correctAnswer: "Responsive"
+    },
+
+    {
+        question:"In JavaScript, what element is used to store multiple values in a single variable?",
+        answers: ["Functions", "Variables", "Arrays", "Objects"],
+        correctAnswer: "Arrays"
+    },
+
+    {
+        question:"What is a JavaScript element that represents either TRUE or FALSE values?",
+        answers: ["Event", "Condition", "Boolean", "Variable"],
+        correctAnswer: "Boolean"
+    },
+
+    {
+        question:"In JavaScript, what element is used to store and manipulate text, usually in multiples?",
+        answers: ["Recorders", "Variables", "Arrays", "Strings"],
+        correctAnswer: "Strings"
+    },
