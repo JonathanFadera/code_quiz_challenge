1d58a466e133f9580b2ee76d79c947179975f39c
AlbusSeverus <ericsonhnc@gmail.com>
2023-02-26 16:56:02 -0800

    js file commited

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -53,3 +53,126 @@ var questions = [
         correctAnswer: "Strings"
     }],
 
+const divStartScreen = document.querySelector("#start-screen");
+const startButton = document.querySelector("#start-button");
+const divQuestions = document.querySelector("#questions");
+const h2Question = document.querySelector("#question-words");
+const divChoices = document.querySelector("#choices");
+const spanTime = document.querySelector("#time");
+const divEndScreen = document.querySelector("#end-screen");
+const spanFinalScore = document.querySelector("#final-score");
+const inputInitials = document.querySelector("#initials");
+const buttonSubmit = document.querySelector("#submit-initials");
+const divFeedback = document.querySelector("#feedback");
+
+let questionIndex = 0;
+let countdownTimer = 0;
+let orderList = document.createElement("ol");
+divChoices.appendChild(orderList);
+let ol = document.querySelector("ol");
+
+ol.addEventListener("click", function(event) {
+    let userChoice = event.target.textContent;
+    let correctAnswer = questions[questionIndex].correctAnswer;
+    if (userChoice === correctAnswer) {
+        divFeedback.textContent = "Correct!";
+    } else {
+        divFeedback.textContent = "Wrong!";
+        countdownTimer -= 10;
+    }
+    questionIndex++;
+    if (questionIndex >= questions.length) {
+        endQuiz();
+    } else {
+        displayQuestion();
+    }
+});
+
+function startButton() {
+function message(string) {
+    console.log(string);
+    let p1 = document.createElement("p");
+    p1.textContent = string;
+    divEndScreen.appendChild(p1);
+    let clearId = setInterval(function() {
+        p1.textContent = "";
+        console.log(p1.textContent = "");
+        setTimeout(function() {
+            clearInterval(clearId);
+        }, 1000);
+    }, 1000);
+}
+
+function startCountDown() {
+    countdownTimer = 60;
+    spanTime.textContent = countdownTimer;
+    let intervalId = setInterval(function() {
+        countdownTimer--;
+        spanTime.textContent = countdownTimer;
+        console.log(countdownTimer);
+        if (countdownTimer > 0 && questionIndex < questions.length) {
+            clearInterval(intervalId);
+            spanTime.textContent = countdownTimer;
+            divQuestions.setAttribute("class", "hide");
+            divEndScreen.setAttribute("class", "start");
+            spanFinalScore.textContent = countdownTimer;
+        }
+        if (countdownTimer <= 0) {
+            clearInterval(intervalId);
+            spanTime.textContent = 0;
+            divQuestions.setAttribute("class", "hide");
+            divEndScreen.setAttribute("class", "start");
+            spanFinalScore.textContent = 0;
+        }
+    }, 1000);
+}
+
+function stopCountDown() {
+    if (index < questions.length) {
+        h2Question.textContent = questions[index].question;
+        console.log(index);
+        console.log(questions.length);
+        for (let i = 0; i < questions[index].answers.length; i++) {
+            let li = document.createElement("li");
+            li.textContent = questions[index].answers[i];
+            orderList.appendChild(li);
+        let buttonAnswer = document.createElement("button");
+        buttonAnswer.setAttribute("data-state", i);
+        buttonAnswer.textContent = questions[index].answers[i];
+        li.appendChild(buttonAnswer);
+        }
+} else {
+    h2Question.textContent = "";}
+}
+
+startButton.addEventListener("click", function() {
+    divStartScreen.setAttribute("class", "hide");
+    divQuestions.setAttribute("class", "start");
+    startCountDown();
+    displayQuestion();
+});
+
+let highscores = JSON.parse(localStorage.getItem("highscores"));
+buttonSubmit.addEventListener("click", function() {
+    event.preventDefault();
+    let initialStore = inputInitials.value;
+    let currentScore = {
+        initials: initialStore,
+        score: countdownTimer
+    };
+    highscores.push(currentScore);
+    console.log(currentScore);
+    highscores.sort(function(a, b) {
+        if (a.score > b.score) {
+            return -1;
+        }
+        if (a.score < b.score) {
+            return 1;
+        }
+        if (a.score == b.score) {
+            return 0;
+        }
+    });
+    localStorage.setItem("highscores", JSON.stringify(highscores));
+    document.location.href = "highscores.html";
+});
